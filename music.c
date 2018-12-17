a1b9d9271dbff34ad0ba51c8d34a742fdc34cf54
oscar <oscar@omnius.omnius.net>
2018-12-17 00:18:16 -0500

    net pulsates

--- music.c
+++ music.c
@@ -13,100 +13,70 @@ typedef struct nodo{
 NodoPtr nodalloc(int cuantos, int capas);
 //void nodfree(NodPtr net);
 int printnet(NodoPtr *net, int capas, int cuantos);
+int propaganet(NodoPtr *net, int capas, int cuantos);
+
+int borrar(NodoPtr net, int capas, int cuantos);
+int borrasums(NodoPtr *net, int capas, int cuantos);
+
+int pulsar(NodoPtr net, int entrada, int capas, int cuantos, int capa_salida);
+
 int main(){
-	int max_node = 2;
+	int cuantos = 2;
 	int capas = 4;
-	NodoPtr red = nodalloc(max_node,capas);
+	NodoPtr red = nodalloc(cuantos,capas);
 
 	int i;
-	for(i=0;i<max_node;i++){
-//		printf("%d\n", red->pesos[i]);
-//		printf("\t%d\n", red->vecinos[i]->pesos[0]);
-	}
 
-	printf("Fine\n");
+//	borrasums(red->vecinos, capas, cuantos);
 
-//	NodoPtr *redarray = red->vecinos;
+//printnet(red->vecinos, capas, cuantos);
 
-	printnet(red->vecinos, capas, max_node);
+//	borrar(red, capas, cuantos);
+
+	printnet(red->vecinos, capas, cuantos);
+	printf("Direct out: %d\n", red->out->sum);
+//	printnet(red->vecinos, capas, cuantos);
+
+	borrar(red, capas, cuantos);
+
+	printf("Salida caluclada: %d\n", pulsar(red, 5, capas, cuantos, 2));
+	borrar(red, capas, cuantos);
+
+	printf("Salida caluclada: %d\n", pulsar(red, 5, capas, cuantos, 2));
+	borrar(red, capas, cuantos);
+
+	printf("Salida caluclada: %d\n", pulsar(red, 5, capas, cuantos, 2));
+
 	return 0;
 }
 
 
 int printnet(NodoPtr *net, int capas, int cuantos){
 	int i,j,k,m,p;
-	k=0;
+	if(net[0]->vecinos[0]->vecinos != NULL){
+				for(m=0;m<cuantos;m++){
+					for(p=0;p<cuantos;p++){
+						printf("sum %d peso %d\n", net[m]->sum, net[m]->pesos[p]);
+						net[m]->vecinos[p]->sum += net[m]->sum * net[m]->pesos[p];
+					}
+				}	
 
-//		printf("%d\n", net[0]->vecinos[0]->sum);
+					printf("\n-\n");
 
-
-//	if(net = NULL){
-//		return 0;
-//	}
-
-//	for(k=0;k<cuantos;k++){
-//		if(net[k]->vecinos[0]->vecinos == NULL){
-//		for(m=0;m<cuantos;m++){
-//			printf("salida %d\n",net[k]->pesos[0]);
-		//	net[k]->vecinos[m]->sum += net[k]->sum * net[k]->pesos[m];
-//		}
-//printf("\n");
-			// resultados finales a la salida out
-
-//			;
-//}
-
-//}
-
-//printf("Finishe\n");
-
-
-//if(net[0]->vecinos == NULL)
-//		printf("Finishhhhhhhhhh\n");
-//else
-if(net[0]->vecinos[0]->vecinos != NULL){
-//	printf("sum %d\n", net[0]->vecinos[0]->sum);
-			for(m=0;m<cuantos;m++){
-				for(p=0;p<cuantos;p++){
-					printf("peso %d\n", net[m]->pesos[p]);
-					net[m]->vecinos[p]->sum += net[m]->sum * net[m]->pesos[p];
-				}
-				printf("\n");
-				// sumarles al acumulador
-			}	
-
-	printnet(net[0]->vecinos, capas, cuantos);
-} else {
-	for(m=0;m<cuantos;m++){
-		printf("peso ultimo %d\n", net[m]->pesos[0]);
-		net[m]->vecinos[0]->sum += net[m]->sum * net[m]->pesos[0];
+		printnet(net[0]->vecinos, capas, cuantos);
+	} else {
+		for(m=0;m<cuantos;m++){
+			printf("sum %d peso ultimo %d\n", net[m]->sum, net[m]->pesos[0]);
+			net[m]->vecinos[0]->sum += net[m]->sum * net[m]->pesos[0];
+		}
+		printf("Salida de la red: %d\n", net[0]->vecinos[0]->sum);
 	}
-	printf("Salida de la red: %d\n", net[0]->vecinos[0]->sum);
-}
-
-//printnet(net[k-1]->vecinos, capas, cuantos);
-
-//		} else { 
-
-//	if(net[k]->vecinos[0]->vecinos != NULL){
-//		for(i = 0; i < cuantos;i++){
-//			printf("%d\n",i);
-//			for(j=0;j<cuantos;j++){
-//				printf("\t%d\n",net[i]->pesos[j]);
-//			}
-//		}
-
-//		printnet(net[++k]->vecinos, --capas, cuantos);
-//	} else {
-//	}
 	return 0;
 }
 
 NodoPtr malla(NodoPtr *net, int capas, int cuantos){
 	int i,j,k,m;
-
 	if(--capas==0){
-		
 		NodoPtr *net2 = (NodoPtr *) malloc(sizeof(NodoPtr) * cuantos);
 		for(j=0;j<cuantos;j++){
 			net2[j] = (NodoPtr) malloc(sizeof(Nodo));
@@ -166,8 +136,7 @@ NodoPtr nodalloc(int cuantos, int capas){
 
 	int i, j, k;
 	NodoPtr net = (NodoPtr) malloc(sizeof(Nodo));
-	net->sum = -1; // entrada de la red
-//	NodoPtr out ;
+	net->sum = 0; // entrada de la red
 
 	NodoPtr *nodarray = (NodoPtr *) malloc(sizeof(NodoPtr) * max_node);
 	int *pesosarray = (int *) malloc(sizeof(int) * max_node);
@@ -195,7 +164,7 @@ NodoPtr nodalloc(int cuantos, int capas){
 		net->pesos[j] = rand();
 
 	for(j=0;j<cuantos;j++)
-		nodarray[j]->sum += net->sum * net->pesos[j];
+		net->vecinos[j]->sum += net->sum * net->pesos[j];
 
 
 
@@ -206,5 +175,77 @@ NodoPtr nodalloc(int cuantos, int capas){
 	return net;
 }
 
+int pulsar(NodoPtr net, int entrada, int capas, int cuantos, int capa_salida){
+	int i,j,k,m,p;
+
+	net->sum = entrada;
+
+	for(j=0;j<cuantos;j++)
+		net->vecinos[j]->sum += net->sum * net->pesos[j];
+
+	 propaganet(net->vecinos, capas, cuantos);
+
+	return net->out->sum;
+}
+
+int propaganet(NodoPtr *net, int capas, int cuantos){
+	int i,j,k,m,p;
+	if(net[0]->vecinos[0]->vecinos != NULL){
+				for(m=0;m<cuantos;m++){
+					for(p=0;p<cuantos;p++){
+						//printf("peso %d\n", net[m]->pesos[p]);
+						net[m]->vecinos[p]->sum += net[m]->sum * net[m]->pesos[p];
+					}
+					//printf("\n");
+				}	
+
+		propaganet(net[0]->vecinos, capas, cuantos);
+	} else {
+		for(m=0;m<cuantos;m++){
+			//printf("peso ultimo %d\n", net[m]->pesos[0]);
+			net[m]->vecinos[0]->sum += net[m]->sum * net[m]->pesos[0];
+		}
+		//printf("Salidurria: %d\n", net[0]->vecinos[0]->sum);
+	}
+	return 0;
+}
+
+int borrar(NodoPtr net, int capas, int cuantos){
+	net->sum = 0;
+	for(int j=0;j<cuantos;j++)
+		net->vecinos[j]->sum = 0;
+
+	 borrasums(net->vecinos, capas, cuantos);
+	return 0;
+	
+}
+
+int borrasums(NodoPtr *net, int capas, int cuantos){
+	int i,j,k,m,p;
+	if(net[0]->vecinos[0]->vecinos != NULL){
+//				for(m=0;m<cuantos;m++){
+
+					for(p=0;p<cuantos;p++){
+//						printf("peso antes %d, ", net[m]->vecinos[p]->sum);
+						net[0]->vecinos[p]->sum = 0;
+//						printf("peso despues %d\n", net[m]->vecinos[p]->sum);
+
+					}
+//				}	
+//					printf("\n-\n");
+
+		borrasums(net[0]->vecinos, capas, cuantos);
+	} else {
+//		for(m=0;m<cuantos;m++){
+//			printf("peso ultimo %d,", net[m]->vecinos[0]->sum );
+			net[0]->vecinos[0]->sum = 0;
+//			printf("peso despues %d\n", net[m]->vecinos[0]->sum);
+
+//		}
+//		printf("Donde sssssssssssssssssssssssiria salida de la red\n");
+		//printf("Salida de la red: %d\n", net[0]->vecinos[0]->sum);
+	}
+	return 0;
+}
 
 
