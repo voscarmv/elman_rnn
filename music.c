e7b0d2e717f0cb9f737bddaa6f05d39ea4c8df9e
oscar <oscar@omnius.omnius.net>
2018-12-20 14:26:18 -0500

    Cleaner code, leaks fixed

--- music.c
+++ music.c
@@ -5,49 +5,6 @@
 #define RANDRANGE 30
 #define NEUROOUTRANGE 10
 
-/*
-
-BIG MEMORY LEAK
-
-==1808== 
-==1808== HEAP SUMMARY:
-==1808==     in use at exit: 60 bytes in 4 blocks
-==1808==   total heap usage: 55 allocs, 51 frees, 7,124 bytes allocated
-==1808== 
-==1808== Searching for pointers to 4 not-freed blocks
-==1808== Checked 69,376 bytes
-==1808== 
-==1808== 4 bytes in 1 blocks are definitely lost in loss record 1 of 4
-==1808==    at 0x4C2BBAF: malloc (vg_replace_malloc.c:299)
-==1808==    by 0x108DF3: malla (music.c:442)
-==1808==    by 0x10936A: nodalloc (music.c:525)
-==1808==    by 0x108A05: main (music.c:160)
-==1808== 
-==1808== 8 bytes in 1 blocks are definitely lost in loss record 2 of 4
-==1808==    at 0x4C2BBAF: malloc (vg_replace_malloc.c:299)
-==1808==    by 0x108DA9: malla (music.c:437)
-==1808==    by 0x10936A: nodalloc (music.c:525)
-==1808==    by 0x108A05: main (music.c:160)
-==1808== 
-==1808== 48 (8 direct, 40 indirect) bytes in 1 blocks are definitely lost in loss record 4 of 4
-==1808==    at 0x4C2BBAF: malloc (vg_replace_malloc.c:299)
-==1808==    by 0x108DE5: malla (music.c:441)
-==1808==    by 0x10936A: nodalloc (music.c:525)
-==1808==    by 0x108A05: main (music.c:160)
-==1808== 
-==1808== LEAK SUMMARY:
-==1808==    definitely lost: 20 bytes in 3 blocks
-==1808==    indirectly lost: 40 bytes in 1 blocks
-==1808==      possibly lost: 0 bytes in 0 blocks
-==1808==    still reachable: 0 bytes in 0 blocks
-==1808==         suppressed: 0 bytes in 0 blocks
-==1808== 
-==1808== ERROR SUMMARY: 3 errors from 3 contexts (suppressed: 0 from 0)
-==1808== ERROR SUMMARY: 3 errors from 3 contexts (suppressed: 0 from 0)
-
-
-*/
-
 typedef struct nodo{
 	int fitness;
 	int sum;
@@ -89,36 +46,9 @@ int feedback(NodoPtr net, int capa_entrada, int capa_memoria, int capas, int cua
 
 int randopes();
 
-// La red ya tiene un rango flexible. Se asigna en NEUROOUTRANGE
-// El rango permitido para los pesos de las dendritas es RANDRANGE
-// El maximo de datos para el dataset de entrada es MAXSAMPLES
-
-// El codigo actual entrega una lista de fitnesses de 10000 redes neuronales.
-// Falta:
-//	1 Escribir la funcion copynet para poder generar un set de redes hijas
-//	2 Escribir la funcion sexnet para combinar dos redes progenitoras
-//	3 Portar el qsort de elmanx.c aqui para ordenar a las redes hijas de acuerdo a su fitness
-
 int comparenets(const void *net1, const void *net2);
 
-/*
-
-qsort(nets, population, sizeof(nets[0]), comparenets);
-
-int comparenets(const void *net1, const void *net2){
-	ElmanPtr *n1 = (ElmanPtr *) net1;
-	ElmanPtr *n2 = (ElmanPtr *) net2;
-	double a = (*n1)->fitness;
-	double b = (*n2)->fitness;
-	if(a > b)
-		return -1;
-	else if(a < b)
-		return 1;
-	else
-		return 0;
-}
-
-*/
+// Clean version
 
 int main(int argc, char **argv){
 	if(argc>0){
@@ -132,9 +62,8 @@ int main(int argc, char **argv){
 
 	int cuantos = 10; // cantidad de neuronas por cada capa
 	int capas = 10; // cantidad de capas
-//	int cuantos = 1, capas = 1;
-	int capa_salida = 5; // capa cuya primer neurona representa la salida
 
+	int capa_salida = 5; // capa cuya primer neurona representa la salida
 	int capa_entrada = 3; // capa a donde ingresan los datos de la malla de memoria
 	int capa_memoria = 9; // capa de la que salen los datos de la malla de memoria
 
@@ -147,288 +76,100 @@ int main(int argc, char **argv){
 	if(capa_memoria <= capa_entrada)
 		return 4;
 
-//	cuantos = 2;
-//	capas = 1;
-
-	int poblacion = 3;
+	int poblacion = 500;
 
 	int input = 1;
 	int output = 1;
 
-//	DatasetPtr data = datalloc(input + output);
 	DatasetPtr data = datalloc(1);
 
 	NodoPtr *redes = (NodoPtr *) malloc(sizeof(NodoPtr) * poblacion);
-//	NodoPtr *redes = (NodoPtr *) malloc(sizeof(NodoPtr) * 3);
-
-
-
-//	redes[0] = nodalloc(9,7);
-
-//	free(redes[0]->vecinos[0]->vecinos[0]);
-//	free(redes[0]->vecinos[0]->vecinos[0]->vecinos[0]->vecinos[0]);
-
-	   //redes[0]->vecinos[0]->vecinos[0]->vecinos[0]->sum);
-/*
-	free(redes[0]->vecinos[0]->vecinos[0]->vecinos[0]->pesos);
-	free(redes[0]->vecinos[0]->vecinos[0]->vecinos[0]->vecinos);
--	free(redes[0]->vecinos[0]->vecinos[0]->vecinos[0]);
-
-	free(redes[0]->vecinos[0]->vecinos[0]->pesos);
-	free(redes[0]->vecinos[0]->vecinos[0]->vecinos);
-	free(redes[0]->vecinos[0]->vecinos[0]);
-
-	free(redes[0]->vecinos[0]->vecinos[1]->pesos);
-	free(redes[0]->vecinos[0]->vecinos[1]->vecinos);
-	free(redes[0]->vecinos[0]->vecinos[1]);
-
-	free(redes[0]->vecinos[0]->pesos);
-	free(redes[0]->vecinos[0]->vecinos);
-	free(redes[0]->vecinos[0]);
-
-	free(redes[0]->vecinos[1]->pesos);
-	free(redes[0]->vecinos[1]->vecinos);
-	free(redes[0]->vecinos[1]);
-
-	
-
-	free(redes[0]->pesos);
-	free(redes[0]->vecinos);
-//	free(redes[0]->out);
-*/	
-//	liberared(redes[0], 9, 7);
-//	free(redes[0]);
-
-//	free(redes);
-
-//	datfree(data);	
-
-//	return 0;
-
-
-//	redes[0] = (NodoPtr) malloc(sizeof(Nodo));
-
-//	struct nodo **vecs = (NodoPtr *) malloc(sizeof(NodoPtr) * 2);
-//	redes[0]->vecinos = vecs;
-
-
-//NodoPtr *nodarray2 = (NodoPtr *) malloc(sizeof(NodoPtr) * cuantos);
-
-//	free(redes[0]->vecinos);
-
-//redes[0]->vecinos = nodarray2;
-//	free(redes[0]->vecinos);
-
-
-	//free(nodarray2);
-
-//	int *pesosarray = (int *) malloc(sizeof(int) * 1);
-//	redes[0]->pesos = pesosarray;
-
-//	free(redes[0]->pesos);
-
-
-
-//	free(redes[0]);
-
-
-
-//	for(i=0;i<poblacion;i++)
-//		liberared(redes[i], cuantos, capas);
-
-//	redes[0] = nodalloc(2,2);
-//	redes[1] = nodalloc(3,3);
-
-
-//	free(redes[0]->vecinos[0]->vecinos[0]->vecinos);
-//	free(redes[0]->vecinos[1]->vecinos[0]->pesos);
-
-//	free(redes[0]->vecinos[0]->pesos);
-//	free(redes[0]->vecinos[1]->pesos);
-
-
-//	free(redes[0]->vecinos[0]->vecinos);
-//	free(redes[0]->vecinos[1]->vecinos);
-
-//	free(redes[0]->vecinos[0]->pesos);
-//	free(redes[0]->vecinos[1]->pesos);
-
-//	free(redes[0]->vecinos);
-//	free(redes[0]->pesos);
-
-//	free(redes);
-
-//	datfree(data);
-
-//	for(i=0;i<poblacion;i++)
-//		liberared(redes[i], cuantos, capas);
-//	free(redes);
-
-
-
 	for(i=0;i<poblacion;i++)
 		redes[i] = nodalloc(cuantos,capas);
 
+	NodoPtr *bebes;
+	int bestanterior = -1;
 
-/*
-
-	for(i=0;i<poblacion;i++){
-		liberared(redes[i], cuantos, capas);
-		free(redes[i]);
-	}
-
-	free(redes);
-	datfree(data);
-
-	return 0;
-*/
-
-
-
-	
-//return 0;
-//	for(i=0;i<data->dataset_l;i++)
-//		printf("%d\n",data->dataset[i][0]);
-NodoPtr *bebes;
-int bestanterior = -1;
-
-for(int x=0;x<2;x++){
-	for(i=0;i<poblacion;i++){
-
-		borrar(redes[i], capas, cuantos);
-		int salida = pulsar(redes[i], data->dataset[0][0], capas, cuantos, capa_salida)+10;
-		feedback(redes[i], capa_entrada, capa_memoria, capas, cuantos);
-
-		int fitness = 0;
-
-		for(j=1;j<data->dataset_l;j++){
-//			printf("Red: %d, Data: %d\n", salida, data->dataset[j][0]);
-			int distancia = salida - data->dataset[j][0];
-			if(distancia < 0)
-				distancia *= -1;
-			fitness += distancia;
-			salida = pulsar(redes[i], salida, capas, cuantos, capa_salida)+10;
-			feedback(redes[i], capa_entrada, capa_memoria, capas, cuantos);
-		}
-		redes[i]->fitness = fitness;
-//		printf("%d\n", fitness);
-	}
-
-	qsort(redes, poblacion, sizeof(redes[0]), comparenets);
-
-	if(bestanterior == -1)
-		bestanterior = redes[0]->fitness;
-	if(redes[0]->fitness < bestanterior){
-		borrar(redes[0], capas, cuantos);
-		int salida = pulsar(redes[0], data->dataset[0][0], capas, cuantos, capa_salida)+10;
-		feedback(redes[0], capa_entrada, capa_memoria, capas, cuantos);
-
-		for(j=1;j<data->dataset_l;j++){
-			printf("Red: %d, Data: %d\n", salida, data->dataset[j][0]);
-			salida = pulsar(redes[0], salida, capas, cuantos, capa_salida)+10;
-			feedback(redes[0], capa_entrada, capa_memoria, capas, cuantos);
-		}
-		bestanterior = redes[0]->fitness;
-	}
-
-//	for(i=0;i<poblacion;i++)
-//		printf("%d\n", redes[i]->fitness);
-		printf("%d: %d\n", x, redes[0]->fitness);
-//	printf("---\n");
-	
-	bebes = (NodoPtr *) malloc(sizeof(NodoPtr) * poblacion);
-
-	for(j=0;j<poblacion;j++){
-		int choose_dad = rand() % ((poblacion*(poblacion+1)) / 2);
-		int choose_mom = rand() % ((poblacion*(poblacion+1)) / 2);
-		NodoPtr papa = NULL;
-		NodoPtr mama = NULL;
-
-		int ctaregresiv = 0;
+	for(int x=0;x<3;x++){
 		for(i=0;i<poblacion;i++){
-			ctaregresiv += poblacion - i;
-//			printf("%d %d %d %d ",redes[i]->fitness, ctaregresiv, choose_dad, choose_mom);
-			if(ctaregresiv > choose_dad){
-//				printf("papa ");
-				papa = redes[i];
+
+			borrar(redes[i], capas, cuantos);
+			int salida = pulsar(redes[i], data->dataset[0][0], capas, cuantos, capa_salida)+10;
+			feedback(redes[i], capa_entrada, capa_memoria, capas, cuantos);
+
+			int fitness = 0;
+
+			for(j=1;j<data->dataset_l;j++){
+				int distancia = salida - data->dataset[j][0];
+				if(distancia < 0)
+					distancia *= -1;
+				fitness += distancia;
+				salida = pulsar(redes[i], salida, capas, cuantos, capa_salida)+10;
+				feedback(redes[i], capa_entrada, capa_memoria, capas, cuantos);
 			}
-			if(ctaregresiv > choose_mom){
-//				printf("mama ");
-				mama = redes[i];
-			}
-			if(papa != NULL && mama != NULL)
-				break;
-//			printf("\n");
+			redes[i]->fitness = fitness;
 		}
-//			printf("\n");
-//		if(j=0){
-//			bebes[j] = netclone(redes[0], cuantos, capas);
-//		} else { 
+
+		qsort(redes, poblacion, sizeof(redes[0]), comparenets);
+
+		if(bestanterior == -1)
+			bestanterior = redes[0]->fitness;
+		if(redes[0]->fitness < bestanterior){
+			borrar(redes[0], capas, cuantos);
+			int salida = pulsar(redes[0], data->dataset[0][0], capas, cuantos, capa_salida)+10;
+			feedback(redes[0], capa_entrada, capa_memoria, capas, cuantos);
+
+			for(j=1;j<data->dataset_l;j++){
+				printf("Red: %d, Data: %d\n", salida, data->dataset[j][0]);
+				salida = pulsar(redes[0], salida, capas, cuantos, capa_salida)+10;
+				feedback(redes[0], capa_entrada, capa_memoria, capas, cuantos);
+			}
+			bestanterior = redes[0]->fitness;
+		}
+
+		printf("%d: %d\n", x, redes[0]->fitness);
+	
+		bebes = (NodoPtr *) malloc(sizeof(NodoPtr) * poblacion);
+
+		for(j=0;j<poblacion;j++){
+			int choose_dad = rand() % ((poblacion*(poblacion+1)) / 2);
+			int choose_mom = rand() % ((poblacion*(poblacion+1)) / 2);
+			NodoPtr papa = NULL;
+			NodoPtr mama = NULL;
+
+			int ctaregresiv = 0;
+			for(i=0;i<poblacion;i++){
+				ctaregresiv += poblacion - i;
+				if(ctaregresiv > choose_dad){
+					papa = redes[i];
+				}
+				if(ctaregresiv > choose_mom){
+					mama = redes[i];
+				}
+				if(papa != NULL && mama != NULL)
+					break;
+			}
 			papa = redes[0]; // el papa siempre va a ser la mejor red
 			bebes[j] = netsex(mama, papa, cuantos, capas);
-//		}
-	}
+		}
 
-//	printf("%d\n",bebes[1]->pesos[0]);
-//return 0;
+		for(i=0;i<poblacion;i++){
+			liberared(redes[i], cuantos, capas);
+			free(redes[i]);
+		}
+		free(redes);
 
-//	for(i = 0; i < poblacion/3; i++){
-//		liberared(bebes[i], cuantos, capas);
-//		free(bebes[i]);
-//		NodoPtr clon = netclone(redes[i], cuantos, capas);
-//		bebes[i] = clon;
-//	}
-
-	for(i=0;i<poblacion;i++){
-		liberared(redes[i], cuantos, capas);
-		free(redes[i]);
-	}
-	free(redes);
-
-	redes = bebes;
+		redes = bebes;
 
 		fflush(NULL);
-
-}
-//////////////////////////////////////////////////////////
-/*	for(i=0;i<poblacion;i++){
-
-		borrar(redes[i], capas, cuantos);
-		int salida = pulsar(redes[i], data->dataset[0][0], capas, cuantos, capa_salida);
-		feedback(redes[i], capa_entrada, capa_memoria, capas, cuantos);
-
-		int fitness = 0;
-
-		for(j=1;j<data->dataset_l;j++){
-//			printf("Red: %d, Data: %d\n", salida, data->dataset[j][0]);
-			int distancia = salida - data->dataset[j][0];
-			if(distancia < 0)
-*				distancia *= -1;
-			fitness += distancia;
-			salida = pulsar(redes[i], salida, capas, cuantos, capa_salida);
-			feedback(redes[i], capa_entrada, capa_memoria, capas, cuantos);
-		}
-		redes[i]->fitness = fitness;
-//		printf("%d\n", fitness);
 	}
-
-	qsort(redes, poblacion, sizeof(redes[0]), comparenets);
-
-	for(i=0;i<poblacion;i++)
-		printf("%d\n", redes[i]->fitness);
-	printf("---\n");
-*/
+//////////////////////////////////////////////////////////
 
 	for(i=0;i<poblacion;i++){
 		liberared(redes[i], cuantos, capas);
 		free(redes[i]);
 	}
 	free(redes);
-//	for(i=0;i<poblacion;i++){
-//		liberared(bebes[i], cuantos, capas);
-//		free(bebes[i]);
-//	}
-//	free(bebes);
 	datfree(data);
 
 	return 0;
